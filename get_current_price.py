65e1cbc4ff1b07ff36c1cf154e848686804660dd
陈希 <chenxi@nat-oitwireless-inside-vapornet100-10-9-147-15.princeton.edu>
2020-04-29 15:42:23 -0400

    update

--- get_current_price.py
+++ get_current_price.py
@@ -1,49 +1,49 @@
 import argparse
-import datetime
+from datetime import date, timedelta
 import pandas_datareader.data as pdr
 # import time
 import openpyxl
 
-stock_list1 = ["IVV", "AAPL", "MSFT", "FB", "BABA"]
-stock_list2 = ["MAR", "BILI", "SE"]
-time = datetime.date.today()
-COL_INDEX = 17
-ROW_INDEX1 = 3
-ROW_INDEX2 = 10
+stock_list1 = ["AAPL", "MSFT", "FB", "BABA", "DAL", "", "BAM", "MAR", "BILI", "SE", "EOG", "", "IVV", "PDD", ""]
+time = date.today()
+while time.weekday() > 4: # Mon-Fri are 0-4
+	time -= timedelta(days=1)
+COL_INDEX = 12
+ROW_INDEX1 = 4
 path = None
 
 def crawl_prices(stock_list):
 	list_len = len(stock_list)
 	prices = []
 	for i in range(list_len):
-		value = pdr.get_data_yahoo(stock_list[i], time, time)
-		price = list(value["Close"].values)
-		prices += price
+		if (stock_list[i] != ""):
+			value = pdr.get_data_yahoo(stock_list[i], time, time)
+			price = list(value["Close"].values)
+			prices.append(price[0])
+		else:
+			prices.append("")
 	return prices
 
 def fill_prices(prices, row_start):
 	wb = openpyxl.load_workbook(filename=path)
-	ws = wb.get_sheet_by_name('Sheet1')
+	ws = wb['Overview']
 	for row, entry in enumerate(prices, start=row_start):
+		if (entry == ""):
+			continue
 		ws.cell(row=row, column=COL_INDEX, value=round(entry,2))
 	wb.save(path)
 
 def crawl_and_fill():
 	prices1 = crawl_prices(stock_list1)
-	prices2 = crawl_prices(stock_list2)
 	fill_prices(prices1, ROW_INDEX1)
-	fill_prices(prices2, ROW_INDEX2)
 
 def main():
-	# while(True):
-	# 	crawl_and_fill()
-	# 	time.sleep(60)
 	crawl_and_fill()
 
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
-	parser.add_argument('--path', '-p', type=str, default='./plan.xlsx')
+	parser.add_argument('--path', '-p', type=str, default='../plan.xlsx')
 	args = parser.parse_args()
 	path = args.path
 
